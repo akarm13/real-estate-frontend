9203fd96ccf7a9e59ecf18203f0278db048a8d0d
Cs-kh <khalidrahim487@gmail.com>
2023-01-15 18:53:06 +0300

    add new component

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -11,6 +11,14 @@ export const Home = () => {
       <Feature />
       <Listing />
       <Faq />
+      <div className="max-w-7xl bg-primary-500 px-6 py-5 mx-auto mt-40 rounded-md ">
+        <div className="w-full flex justify-between items-center">
+          <p className="text-white font-semibold text-xl">Looking to find your next home?</p>
+          <div className="rounded-3xl px-7 py-3 bg-white">
+            <p className="text-primary-500">Find your next home</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
