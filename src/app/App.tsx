70ef5bc0469f164e0bda39c42bb418081526d66d
Akar Muhamad <akarmuhamad13@gmail.com>
2023-01-15 11:31:44 +0300

    Setup Tailwind Config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,7 +8,7 @@ import { ScrollToTop } from "../components/ScrollToTop";
 
 function App() {
   return (
-    <main className="bg-[#FEFEFF]">
+    <main className="bg-[#FEFEFF] font-sans">
       <Navigation />
       <ScrollToTop />
       <Routes>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,38 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#DEDBFF",
+          200: "#BDB8FF",
+          300: "#9C94FF",
+          400: "#7C70FF",
+          500: "#5B4DFF",
+          600: "#493DCC",
+          700: "#362E99",
+          800: "#241F66",
+          900: "#120F33",
+          background: "rgba(91, 77, 255, 0.1)",
+        },
+        primaryText: "#000929",
+        secondaryText: "#4D5461",
+        gray: {
+          100: "#ECEBEF",
+          200: "#D8D8DF",
+          300: "#C5C4CF",
+          400: "#B2B0BF",
+          500: "#9E9DAF",
+          600: "#7F7D8C",
+          700: "#5F5E69",
+          800: "#3F3F46",
+          900: "#201F23",
+        },
+      },
+      fontFamily: {
+        sans: ["Plus Jakarta Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
