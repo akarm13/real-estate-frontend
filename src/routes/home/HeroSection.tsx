9a8878bf70163f80673d3d51e14a8112f89a9350
Cs-kh <khalidrahim487@gmail.com>
2023-01-21 19:33:29 +0300

    remove width of text

--- src/routes/home/HeroSection.tsx
+++ src/routes/home/HeroSection.tsx
@@ -6,7 +6,7 @@ export const HeroSection = () => {
     <div className="flex-col-reverse  items-center max-w-7xl flex md:flex-row md:justify-between md:items-center mx-auto">
       <div className="flex flex-col gap-y-8 w-5/6">
         <div className="">
-          <h1 className="text-3xl w-2/3 font-semibold md:text-6xl text-[#120F33] leading-tight">
+          <h1 className="text-3xl  font-semibold md:text-6xl text-[#120F33] leading-tight">
             Finding your next home has never been easier
           </h1>
           <p className=" text-[#120F33] text-xl md:text-3xl mt-4 ">
@@ -15,10 +15,10 @@ export const HeroSection = () => {
         </div>
         <div className="flex gap-x-6">
           <Button onClick={() => console.log("login")} variant="primary">
-            Browse Listings
+            Listings
           </Button>
           <Button onClick={() => console.log("sign up")} variant="secondary">
-            Browse Agents
+            Agents
           </Button>
         </div>
       </div>
