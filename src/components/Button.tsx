d9ad4e992243bd1d093b1eb8bf77dc86652f35b8
Cs-kh <khalidrahim487@gmail.com>
2023-01-23 00:02:59 +0300

    remove margin in secondary btn

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 type Props = {
   children: React.ReactNode;
-  variant: "primary" | "secondary" |"none";
+  variant: "primary" | "secondary" | "none";
   onClick: () => void;
 };
 export const Button = ({ onClick, children, variant }: Props) => {
@@ -21,7 +21,7 @@ export const Button = ({ onClick, children, variant }: Props) => {
     return (
       <button
         onClick={onClick}
-        className="bg-white  text-white md:font-bold md:py-2 py-[5px] px-[10px] md:px-4 rounded"
+        className="bg-white  text-secondaryText md:font-bold md:py-2 py-[5px] px-[10px] md:px-4 rounded"
       >
         {children}
       </button>
@@ -31,7 +31,7 @@ export const Button = ({ onClick, children, variant }: Props) => {
   return (
     <button
       onClick={onClick}
-      className="md:bg-white text-white md:text-black bg-primary-500 border-2 border-primary-background md:hover:bg-gray-100 md:font-bold md:py-2 md:px-4 py-[5px] px-[10px]  ml-5 md:ml-1 rounded"
+      className="bg-white  text-black  border-2 border-primary-background md:hover:bg-gray-100 md:font-bold md:py-2 md:px-4 py-[5px] px-[10px]    rounded"
     >
       {children}
     </button>
