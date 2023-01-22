4fb874ffd5cf9399a290fdec56b8b425052d7416
Akar Muhamad <akarmuhamad13@gmail.com>
2023-01-23 02:49:15 +0300

    Changing padding of button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,14 +4,11 @@ type Props = {
   onClick: () => void;
 };
 export const Button = ({ onClick, children, variant }: Props) => {
-
-
-
   if (variant === "primary") {
     return (
       <button
         onClick={onClick}
-        className="bg-primary-500 hover:bg-primary-600 text-white md:font-bold md:py-2 py-[5px] px-[10px] md:px-4 rounded"
+        className="bg-primary-500 hover:bg-primary-600 text-white md:font-bold md:py-2 py-2 px-4 md:px-4 rounded"
       >
         {children}
       </button>
