1fefba898bae3f2f8f23c300e1f8bbb5b7b5e51b
Cs-kh <khalidrahim487@gmail.com>
2023-01-24 23:28:11 +0300

    add color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -25,6 +25,7 @@ module.exports = {
         },
         primaryText: "#000929",
         secondaryText: "#4D5461",
+        searchBackground: "#F1F0F6",
         gray: {
           100: "#ECEBEF",
           200: "#D8D8DF",
