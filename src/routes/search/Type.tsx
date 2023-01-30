4a2185c1594a96c2a1834af85baec49508209a60
Cs-kh <khalidrahim487@gmail.com>
2023-01-30 16:23:38 +0300

    add type to style page

---
+++ src/routes/search/Type.tsx
@@ -0,0 +1,26 @@
+export const Type = () => {
+    return (
+        <div className="">
+
+            <h1 className="mb-4 capitalize text-lg text-primaryText font-semibold">Type</h1>
+            <ul className=" text-sm font-medium  flex gap-y-3 flex-col ">
+                <li className="w-full ">
+                    <div className="flex items-center ">
+                        <input id="rent-checkbox" type="checkbox" value="" className="w-5 h-5 text-blue-600 bg-gray-100  border-0  rounded focus:ring-0 focus:ring-offset-0" />
+                        <label htmlFor="rent-checkbox" className="w-full  ml-2 text-base  text-primary-900 font-normal">Rent</label>
+                    </div>
+                </li>
+
+                <li className="w-full  ">
+                    <div className="flex items-center ">
+                        <input id="sale-checkbox" type="checkbox" value="" className="w-5 h-5 text-blue-600 bg-gray-100  border-0  rounded focus:ring-0 focus:ring-offset-0" />
+                        <label htmlFor="sale-checkbox" className="w-full  ml-2 text-base  text-primary-900 font-normal">Sale</label>
+                    </div>
+                </li>
+
+
+            </ul>
+
+        </div>
+    )
+} 
