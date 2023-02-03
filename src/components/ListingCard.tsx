c35c52aaace8fdf8926e15a1af2d6e4a3ed4d1b4
Akar <akarmuhamad13@gmail.com>
2023-02-03 19:27:03 +0300

    Change it to link

--- src/components/ListingCard.tsx
+++ src/components/ListingCard.tsx
@@ -8,6 +8,7 @@ import { ReactComponent as BedroomIcon } from "../assets/icons/listing/bedroom.s
 import { ReactComponent as BathroomIcon } from "../assets/icons/listing/bathroom.svg";
 import { ReactComponent as AreaIcon } from "../assets/icons/listing/area.svg";
 import { ReactComponent as FeaturedStarIcon } from "../assets/icons/listing/featured-star.svg";
+import { Link } from "react-router-dom";
 
 type Props = {
   price: number;
@@ -30,47 +31,48 @@ export const ListingCard = ({
   type,
 }: Props) => {
   return (
-    <div className="flex flex-col">
-      <img
-        src={house}
-        alt="house"
-        className="h-[170px] w-full rounded-md object-cover"
-      />
-      <div className="flex flex-col gap-y-2 px-5 items-baseline bg-white border border-primary-background py-6 rounded-md relative ">
-        <span className="text-primary-500 font-bold text-lg">${price}</span>
-        <h3 className="text-primary-900 font-semibold text-lg h-[72px]">
-          {title}
-        </h3>
-        <p className="flex gap-x-2 p-0 m-0 items-center mt-2">
-          <LocationIcon />
-
-          <span className="text-secondaryText">{address}</span>
-        </p>
-        <span className="bg-gray-50 text-gray-800 px-4 py-2 rounded-md capitalize my-2">
-          {type}
-        </span>
-        <hr className="w-full mx-auto my-2 bg-primary-background" />
-        <div className="flex justify-between w-full mt-2">
-          <PropertyDetail
-            icon={<BedroomIcon />}
-            value={bedrooms}
-            text={"bd."}
-          />
-          <PropertyDetail
-            icon={<BathroomIcon />}
-            value={bathrooms}
-            text={"bth."}
-          />
-          <PropertyDetail icon={<AreaIcon />} value={area} text={"m²"} />
-        </div>
-        <div className="absolute bg-primary-500 flex -top-7 -left-1 items-center gap-x-2 py-2 px-4 rounded-md">
-          <FeaturedStarIcon width={16} height={16} />
-          <span className="text-white uppercase text-sm font-semibold">
-            {status}
+    <Link to="/listings/93103109">
+      <div className="flex flex-col transition">
+        <img
+          src={house}
+          alt="house"
+          className="h-[170px] w-full rounded-md object-cover"
+        />
+        <div className="flex flex-col gap-y-2 px-5 items-baseline bg-white border border-primary-background py-6 rounded-md relative ">
+          <span className="text-primary-500 font-bold text-lg">${price}</span>
+          <h3 className="text-primary-900 font-semibold text-lg h-[72px]">
+            {title}
+          </h3>
+          <p className="flex gap-x-2 p-0 m-0 items-center mt-2">
+            <LocationIcon />
+            <span className="text-secondaryText">{address}</span>
+          </p>
+          <span className="bg-gray-50 text-gray-800 px-4 py-2 rounded-md capitalize my-2">
+            {type}
           </span>
+          <hr className="w-full mx-auto my-2 bg-primary-background" />
+          <div className="flex justify-between w-full mt-2">
+            <PropertyDetail
+              icon={<BedroomIcon />}
+              value={bedrooms}
+              text={"bd."}
+            />
+            <PropertyDetail
+              icon={<BathroomIcon />}
+              value={bathrooms}
+              text={"bth."}
+            />
+            <PropertyDetail icon={<AreaIcon />} value={area} text={"m²"} />
+          </div>
+          <div className="absolute bg-primary-500 flex -top-7 -left-1 items-center gap-x-2 py-2 px-4 rounded-md">
+            <FeaturedStarIcon width={16} height={16} />
+            <span className="text-white uppercase text-sm font-semibold">
+              {status}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
