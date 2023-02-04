db782115eb3de6ac963258e652dd888c8b3fd4df
Akar Muhamad <akarmuhamad13@gmail.com>
2023-02-04 04:11:56 +0300

    Make the vertical padding smaller on mobile

--- src/routes/search/InputSearch.tsx
+++ src/routes/search/InputSearch.tsx
@@ -9,7 +9,7 @@ export const InputSearch = () => {
   });
   return (
     <div className="flex justify-between items-center w-full">
-      <div className="bg-searchBackground p-2 px-4 w-full mx-auto md:mx-0 lg:w-[700px] rounded-lg flex justify-between">
+      <div className="bg-searchBackground py-1 md:py-2 md: px-4 w-full mx-auto md:mx-0 lg:w-[700px] rounded-lg flex justify-between">
         {isSmall ? (
           <>
             <select className="bg-white text-black px-4 py-1 flex gap-x-4 w-32 text-lg border-none rounded-lg outline-none focus:ring-0 focus:ring-offset-0">
@@ -23,7 +23,7 @@ export const InputSearch = () => {
             <input
               type="text"
               placeholder="City or address"
-              className="text-center  bg-transparent border-none focus:ring-0 focus:ring-offset-0"
+              className="text-center bg-transparent border-none focus:ring-0 focus:ring-offset-0"
             />
             <SearchIcon className="self-center" />
           </>
@@ -35,12 +35,11 @@ export const InputSearch = () => {
           />
         )}
       </div>
-      {isSmall ? (
+
+      {isSmall && (
         <div className="flex items-center border-primary-background  px-9 py-3 border-2 rounded-lg">
           <LocationIcon />
         </div>
-      ) : (
-        ""
       )}
     </div>
   );
