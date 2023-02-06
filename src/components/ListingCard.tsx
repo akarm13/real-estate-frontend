94736bd52910d19f4faecb74dc9b05efb3523991
Akar <akarmuhamad13@gmail.com>
2023-02-06 19:23:13 +0300

    Simplify the listing card

--- src/components/ListingCard.tsx
+++ src/components/ListingCard.tsx
@@ -30,49 +30,52 @@ export const ListingCard = ({
   title,
   type,
 }: Props) => {
+  // If type is sale then "bg-primary-800 text-white" else "bg-primary-200 text-primary-800"
+  const typeColor =
+    type === "sale"
+      ? "bg-primary-800 text-white"
+      : "bg-primary-100 text-primary-800";
+
   return (
-   
-      <div className="flex flex-col transition hover:scale-105 duration-200">
-        <img
-          src={house}
-          alt="house"
-          className="h-[170px] w-full rounded-md object-cover"
-        />
-        <div className="flex flex-col gap-y-2 px-5 items-baseline bg-white border border-primary-background py-6 rounded-md relative ">
-          <span className="text-primary-500 font-bold text-lg">${price}</span>
-          <h3 className="text-primary-900 font-semibold text-lg h-[72px]">
-            {title}
-          </h3>
-          <p className="flex gap-x-2 p-0 m-0 items-center mt-2">
-            <LocationIcon />
-            <span className="text-secondaryText">{address}</span>
-          </p>
-          <span className="bg-gray-50 text-gray-800 px-4 py-2 rounded-md capitalize my-2">
-            {type}
+    <div className="flex flex-col transition hover:scale-105 duration-200 relative">
+      <img
+        src={house}
+        alt="house"
+        className="h-[170px] w-full rounded-md object-cover"
+      />
+      <span
+        className={`absolute left-2 px-4 py-2 rounded-md uppercase my-2 font-medium ${typeColor} text-sm`}
+      >
+        {type}
+      </span>
+      <div className="flex flex-col gap-y-2 px-5 items-baseline bg-white border border-primary-background py-6 rounded-md relative ">
+        <span className="text-primary-500 font-bold text-lg">${price}</span>
+        <h3 className="text-primary-900 font-semibold text-lg h-[64px]">
+          {title}
+        </h3>
+
+        <hr className="w-full mx-auto my-2 bg-primary-background" />
+        <div className="flex justify-between w-full mt-2">
+          <PropertyDetail
+            icon={<BedroomIcon />}
+            value={bedrooms}
+            text={"bd."}
+          />
+          <PropertyDetail
+            icon={<BathroomIcon />}
+            value={bathrooms}
+            text={"bth."}
+          />
+          <PropertyDetail icon={<AreaIcon />} value={area} text={"m²"} />
+        </div>
+        <div className="absolute bg-primary-500 flex -top-7 -left-1 items-center gap-x-2 py-2 px-4 rounded-md">
+          <FeaturedStarIcon width={16} height={16} />
+          <span className="text-white uppercase text-sm font-semibold">
+            {status}
           </span>
-          <hr className="w-full mx-auto my-2 bg-primary-background" />
-          <div className="flex justify-between w-full mt-2">
-            <PropertyDetail
-              icon={<BedroomIcon />}
-              value={bedrooms}
-              text={"bd."}
-            />
-            <PropertyDetail
-              icon={<BathroomIcon />}
-              value={bathrooms}
-              text={"bth."}
-            />
-            <PropertyDetail icon={<AreaIcon />} value={area} text={"m²"} />
-          </div>
-          <div className="absolute bg-primary-500 flex -top-7 -left-1 items-center gap-x-2 py-2 px-4 rounded-md">
-            <FeaturedStarIcon width={16} height={16} />
-            <span className="text-white uppercase text-sm font-semibold">
-              {status}
-            </span>
-          </div>
         </div>
       </div>
-    
+    </div>
   );
 };
 
