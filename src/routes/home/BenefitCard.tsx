64a611717ae2a409c1a41100979842f785c484ea
Akar <akarmuhamad13@gmail.com>
2023-02-07 22:51:20 +0300

    Change the benefit card to primary color

--- src/routes/home/BenefitCard.tsx
+++ src/routes/home/BenefitCard.tsx
@@ -9,7 +9,7 @@ export const BenefitCard = ({ title, description }: Props) => {
   return (
     <div className="bg-white border border-primary-background text-center  flex flex-col items-center gap-y-6 py-[40px] rounded-2xl h-80">
       <div className="bg-primary-background p-4 flex items-center justify-center rounded-2xl">
-        <SearchIcon />
+        <SearchIcon fill="currentColor" className="text-primary-500" />
       </div>
       <h1 className="text-primary-900 font-semibold text-xl">{title}</h1>
       <p className="w-10/12 mx-auto text-secondaryText">{description}</p>
