13bec6feb78b07d3ec891110877d3a2da0bbcb84
Akar Muhamad <akarmuhamad13@gmail.com>
2023-02-11 01:08:04 +0300

    Add snap-center to the link

--- src/routes/listings/SimilarListingsSection.tsx
+++ src/routes/listings/SimilarListingsSection.tsx
@@ -9,10 +9,14 @@ export const SimilarListingsSection = () => {
     <div className="flex flex-col gap-4 mt-14 ">
       <h2 className="text-2xl font-semibold">Similar Listings</h2>
 
-      <div className="lg:grid  lg:grid-cols-4 lg:gap-x-7  pt-4 flex gap-x-4   snap-x overflow-x-auto snap-proximity lg:snap-none ">
+      <div className="lg:grid  lg:grid-cols-4 lg:gap-x-7  pt-4 flex gap-x-4 snap-x overflow-x-auto snap-proximity lg:snap-none ">
         {similarListings.map((listing) => {
           return (
-            <Link to={`/listings/${listing.id}`} key={listing.id}>
+            <Link
+              to={`/listings/${listing.id}`}
+              key={listing.id}
+              className="snap-center"
+            >
               <ListingCard {...listing} key={listing.id} />
             </Link>
           );
