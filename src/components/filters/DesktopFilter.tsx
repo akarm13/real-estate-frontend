92a16f9da9aa9dbcfb4e316e9ab9f4623f7684d8
Cs-kh <khalidrahim487@gmail.com>
2023-02-18 15:43:44 +0300

    add keyword search to desktop filter but the style is not good

--- src/components/filters/DesktopFilter.tsx
+++ src/components/filters/DesktopFilter.tsx
@@ -4,8 +4,50 @@ import { HomeSize } from "../../routes/search/HomeSize";
 import { Button } from "../Button";
 import { PriceInput } from "./PriceInput";
 import { Type } from "../../routes/search/TypeOfHouse";
+import CreatableSelect from "react-select/creatable";
+import { StylesConfig } from "react-select";
+import makeAnimated from "react-select/animated";
+
 
 export const DesktopFilter = () => {
+  const colourStyles: StylesConfig = {
+    control: (styles, { isFocused }) => ({
+      ...styles,
+      backgroundColor: "white",
+      borderRadius: 8,
+      minHeight: 40,
+      borderColor: isFocused ? "#5B4DFF" : "#ECEBEF",
+    }),
+    option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+      return {
+        ...styles,
+        backgroundColor: isFocused ? "rgba(91, 77, 255, 0.1)" : "white",
+        color: isFocused ? "rgba(91, 77, 255, 1)" : "rgba(0, 0, 0, 0.8)",
+      };
+    },
+    multiValue: (styles, { data }) => {
+      return {
+        ...styles,
+        backgroundColor: "rgba(91, 77, 255, 0.1)",
+        borderRadius: 8,
+        padding: "4px 8px",
+        height: 32,
+        display: "flex",
+        alignItems: "center",
+      };
+    },
+    multiValueLabel: (styles, { data }) => ({
+      ...styles,
+      color: "rgba(91, 77, 255, 1)",
+      fontWeight: 500,
+    }),
+    menu: (styles) => ({
+      ...styles,
+      borderRadius: 8,
+      backgroundColor: "white",
+    }),
+  };
+  const animatedComponents = makeAnimated();
   return (
     <div className="py-6 px-5 border-primary-background border-2 flex flex-col gap-y-6 rounded-lg">
       <Category />
@@ -22,6 +64,32 @@ export const DesktopFilter = () => {
       <NumOfRoom />
       <hr />
       <HomeSize />
+      <hr />
+
+      <div className="p-4">
+        <div className="flex items-center">
+          <h1 className="text-base font-semibold text-primaryText flex flex-col">
+            <span>Keyword</span>
+            <span className="text-gray-500 font-normal mt-1">
+              (Search by title, ameneties, etc.)
+            </span>
+          </h1>
+        </div>
+        <div className="grid items-center mt-4 flex-wrap mb-12">
+          {/* Put the dropdown list above */}
+          <CreatableSelect
+            options={[]}
+            isMulti
+            placeholder="Eg. Balcony, Swimming pool, etc."
+            styles={colourStyles}
+            components={{
+              ...animatedComponents,
+              DropdownIndicator: () => null,
+              IndicatorSeparator: () => null,
+            }}
+          />
+        </div>
+      </div>
       <hr />
       <Button onClick={() => console.log("login")} variant="primary">
         APPLY FILTERS
