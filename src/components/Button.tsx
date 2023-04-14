bd484647e6245dd17517e452bca1242e4e34a95c
Akar <akarmuhamad13@gmail.com>
2023-04-14 21:42:26 +0300

    feat: Add loading indicator as a prop (#1)

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,40 @@
+import { ClipLoader } from "react-spinners";
+
 type Props = {
   children: React.ReactNode;
   variant: "primary" | "secondary" | "none";
   onClick: () => void;
   className?: string;
+  isLoading?: boolean;
 };
-export const Button = ({ onClick, children, variant, className }: Props) => {
+export const Button = ({
+  onClick,
+  children,
+  variant,
+  className,
+  isLoading,
+}: Props) => {
+  const loader = (
+    <div className="flex justify-center items-center">
+      <span className="pr-2">Please wait...</span>
+      <ClipLoader
+        color={variant === "primary" ? "#FFF" : "#36d7b7"}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+        size={18}
+      />
+    </div>
+  );
+
   if (variant === "primary") {
     return (
       <button
         onClick={onClick}
-        className={`bg-primary-500 hover:bg-primary-600 font-semibold text-white md:py-2 py-2 px-4 md:px-4 rounded-lg transition ${className}`}
+        className={`bg-primary-500 hover:bg-primary-600 font-semibold text-white md:py-2 py-2 px-4 md:px-4 rounded-lg transition ${className} ${
+          isLoading ? "pointer-events-none opacity-80" : ""
+        }`}
       >
-        {children}
+        {isLoading ? loader : children}
       </button>
     );
   }
@@ -21,7 +44,7 @@ export const Button = ({ onClick, children, variant, className }: Props) => {
         onClick={onClick}
         className={`bg-white  text-secondaryText py-2 px-4 md:py-2 md:px-4 rounded-lg ${className}`}
       >
-        {children}
+        {isLoading ? loader : children}
       </button>
     );
   }
@@ -31,7 +54,7 @@ export const Button = ({ onClick, children, variant, className }: Props) => {
       onClick={onClick}
       className={`bg-white  text-black  border-2 border-primary-background md:hover:bg-gray-100 py-2 px-4 md:py-2 md:px-4 rounded-lg ${className}`}
     >
-      {children}
+      {isLoading ? loader : children}
     </button>
   );
 };
