3899edd2da1471c4cd8fc848bee2c7e9758a2f07
Akar <akarmuhamad13@gmail.com>
2023-05-02 00:47:27 +0300

    Add tooltip and stuff

--- src/components/ListingCard.tsx
+++ src/components/ListingCard.tsx
@@ -1,10 +1,17 @@
 import { Listing } from "../types/listing";
-
+import { ReactComponent as StarFilled } from "../assets/icons/listing/star-filled.svg";
+import { ReactComponent as StarOutline } from "../assets/icons/listing/star-outline.svg";
 import { ReactComponent as AreaIcon } from "../assets/icons/listing/area.svg";
 import { ReactComponent as BathroomIcon } from "../assets/icons/listing/bathroom.svg";
 import { ReactComponent as BedroomIcon } from "../assets/icons/listing/bedroom.svg";
 import { ReactComponent as FeaturedStarIcon } from "../assets/icons/listing/featured-star.svg";
 import { ReactComponent as LocationIcon } from "../assets/icons/listing/location.svg";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./Tooltip";
 
 export const ListingCard = ({
   price,
@@ -15,9 +22,12 @@ export const ListingCard = ({
   title,
   type,
   images,
+  isFavorited,
 }: Listing) => {
   return (
     <div className="listing-card relative my-4 flex snap-center flex-col rounded-lg transition duration-200 lg:my-0">
+      {/* Overlay */}
+
       <div className="overflow-hidden rounded-lg">
         <img
           src={images[0]}
@@ -25,7 +35,26 @@ export const ListingCard = ({
           className="h-[170px] w-full rounded-lg object-cover"
         />
       </div>
-      <TypeBadge type={type} className="absolute top-2 left-4" />
+
+      {isFavorited !== undefined ? (
+        <div className="absolute top-2 right-2 p-2 rounded-lg bg-primary-500">
+          {isFavorited ? (
+            <StarFilled
+              width={20}
+              height={20}
+              className="fill-current text-yellow-400"
+            />
+          ) : (
+            <StarOutline
+              width={20}
+              height={20}
+              className="fill-current text-primary text-yellow-200"
+            />
+          )}
+        </div>
+      ) : null}
+
+      <TypeBadge type={type} className="absolute top-0 left-0" />
       <div className="relative flex flex-col items-baseline gap-y-2 rounded-lg border border-primary-background bg-white px-5 py-6 ">
         <span className="text-lg font-bold text-primary-500">
           {type === "sale" ? `$${price}` : `$${price}/mo`}
@@ -36,7 +65,23 @@ export const ListingCard = ({
         <p className="m-0 flex items-center gap-x-2 p-0">
           <LocationIcon />
           <span className="lg:text-md text-sm text-secondaryText">
-            {location.address + " " + location.city}
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  {location.address.length > 20 ? (
+                    <>
+                      {location.address.substring(0, 30)}
+                      <span className="text-primary-500">...</span>
+                    </>
+                  ) : (
+                    location.address
+                  )}
+                </TooltipTrigger>
+                <TooltipContent>
+                  {location.address}, {location.city}
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
           </span>
         </p>
 
@@ -54,9 +99,20 @@ export const ListingCard = ({
           />
           <PropertyDetail icon={<AreaIcon />} value={area} text={"m²"} />
         </div>
-        <div className="absolute -top-7 -left-1 flex items-center gap-x-2 rounded-lg bg-primary-500 py-2 px-4">
-          <FeaturedStarIcon width={16} height={16} />
-          <span className="text-sm font-semibold uppercase text-white">
+
+        <div
+          className={`absolute -top-7 -left-1 flex items-center gap-x-2 rounded-lg ${
+            status === "featured" ? "bg-primary-500" : "bg-primary-50"
+          } py-2 px-4`}
+        >
+          {status === "featured" ? (
+            <FeaturedStarIcon width={16} height={16} fill="#FFF" />
+          ) : null}
+          <span
+            className={`text-sm font-semibold uppercase ${
+              status === "featured" ? "text-white" : "text-gray-800"
+            }`}
+          >
             {status}
           </span>
         </div>
@@ -90,7 +146,7 @@ export const TypeBadge = ({ type, className }: TypeBadgeProps) => {
   const typeColor =
     type === "sale"
       ? "bg-primary-800 text-primary-100"
-      : "bg-primary-100 text-primary-800";
+      : "bg-primary-50 text-primary-800";
 
   return (
     <span
