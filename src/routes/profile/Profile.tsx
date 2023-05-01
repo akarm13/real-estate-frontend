4c0a6d887cb240f0e7b470b3669fab4998634d51
Akar Muhamad <akarmuhamad13@gmail.com>
2023-05-01 03:02:41 +0300

    Add user avatar and full name (#9)

--- src/routes/profile/Profile.tsx
+++ src/routes/profile/Profile.tsx
@@ -1,12 +1,14 @@
 import { ListMinus, Settings, Star, UserCog, UserIcon } from "lucide-react";
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { NavLink, Outlet, useLocation, useNavigate } from "react-router-dom";
+import { selectAuth } from "../../store/slices/auth";
 
 export const Profile = () => {
   const getActiveClassName = ({ isActive }: any) =>
     isActive
-      ? "text-primary-500 border-b-3 border-primary-500 flex items-center gap-x-4 mx-auto border-b pb-2"
-      : "text-gray-600 hover:text-primary-500 flex items-center gap-x-4 mx-auto border-b-3 border-gray-300 pb-2";
+      ? "text-primary-500 border-b-2 border-primary-500 flex items-center gap-x-4 pb-2"
+      : "text-gray-600 hover:text-primary-500 flex items-center gap-x-4 border-b-3 border-gray-300 pb-2";
 
   const navigate = useNavigate();
 
@@ -15,10 +17,24 @@ export const Profile = () => {
   useEffect(() => {
     if (location.pathname === "/profile") navigate("/profile/edit");
   }, [navigate, location.pathname]);
+
+  const { user } = useSelector(selectAuth);
+
   return (
     <div className="w-full pt-32">
       <div className="container mx-auto flex flex-col">
-        <nav className="flex items-center gap-x-8 mx-auto border-b border-gray-100 overflow-x-scroll w-full lg:w-auto flex-nowrap">
+        <div className="flex items-center gap-x-4 mb-8 lg:w-3/4 xl:w-4/6 mx-auto mt-8">
+          <img
+            src={user?.avatar || "/path/to/default/avatar.png"}
+            alt="User Avatar"
+            className="h-16 w-16 rounded-full object-cover"
+          />
+          <div className="text-lg">
+            <h1 className="font-semibold">{user?.fullName}</h1>
+            <p className="text-gray-500 capitalize">{user?.role}</p>
+          </div>
+        </div>
+        <nav className="flex items-center gap-x-8 mb-8 lg:w-3/4 xl:w-4/6 mx-auto mt-8 border-gray-100 overflow-x-scroll">
           <NavLink to="edit" className={getActiveClassName}>
             <UserCog className="h-6 w-6" />
             <span className="whitespace-nowrap">Edit Profile</span>
@@ -37,7 +53,7 @@ export const Profile = () => {
           </NavLink>
         </nav>
 
-        <div className="mt-8">
+        <div className="w-full lg:w-3/4 xl:w-4/6 mx-auto mt-8">
           <Outlet />
         </div>
       </div>
