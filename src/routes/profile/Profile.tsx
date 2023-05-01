035e6e88514a220a7b66c6325eef6c91badf7a61
Akar <akarmuhamad13@gmail.com>
2023-05-01 22:06:03 +0300

    Fix the jumping issue (#9)

--- src/routes/profile/Profile.tsx
+++ src/routes/profile/Profile.tsx
@@ -7,8 +7,8 @@ import { selectAuth } from "../../store/slices/auth";
 export const Profile = () => {
   const getActiveClassName = ({ isActive }: any) =>
     isActive
-      ? "text-primary-500 border-b-2 border-primary-500 flex items-center gap-x-4 pb-2"
-      : "text-gray-600 hover:text-primary-500 flex items-center gap-x-4 border-b-3 border-gray-300 pb-2";
+      ? "text-primary-500 border-primary-500 flex items-center gap-x-4 pb-2 border-b-2"
+      : "text-gray-600 hover:text-primary-500 flex items-center gap-x-4 border-b-2 border-transparent pb-2";
 
   const navigate = useNavigate();
 
@@ -23,17 +23,20 @@ export const Profile = () => {
     <div className="w-full pt-20 relative">
       <div className="container mx-auto flex border-r-primary-background bg-white">
         <nav className="hidden fixed md:flex flex-col gap-y-4 w-72 border-r border-primary-background h-full overflow-y-auto pt-12">
-          <div className="flex items-center gap-x-4 flex-col mb-8">
-            <img
-              src={user?.avatar || "https://i.pravatar.cc/150?img=68"}
-              alt="User Avatar"
-              className="h-12 w-12 rounded-full object-cover"
-            />
-            <div className="text-center mt-4">
-              <h1 className="font-semibold text-lg">{user?.fullName}</h1>
-              <p className="text-gray-500 capitalize">{user?.role}</p>
+          <div className="flex items-center gap-y-8 flex-col mb-8">
+            <div className="flex flex-col">
+              <img
+                src={user?.avatar || "https://i.pravatar.cc/150?img=68"}
+                alt="User Avatar"
+                className="h-12 w-12 rounded-full object-cover"
+              />
+              <div className="text-center mt-4">
+                <h1 className="font-semibold text-lg">{user?.fullName}</h1>
+                <p className="text-gray-500 capitalize">{user?.role}</p>
+              </div>
             </div>
           </div>
+
           <NavLink to="edit" className={getActiveClassName}>
             <UserCog className="h-6 w-6" />
             <span className="whitespace-nowrap">Edit Profile</span>
