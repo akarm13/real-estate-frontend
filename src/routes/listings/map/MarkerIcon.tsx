b31bb43d593d00ed3b46c690d19ff2d57fa0a93f
Akar Muhamad <akarmuhamad13@gmail.com>
2023-05-03 10:59:29 +0300

    fix ts error

--- src/routes/listings/map/MarkerIcon.tsx
+++ src/routes/listings/map/MarkerIcon.tsx
@@ -1,4 +1,4 @@
-export const MarkerIcon = ({ isHovered }: { isHovered: boolean }) => {
+export const MarkerIcon = ({ isHovered }: { isHovered?: boolean }) => {
   return (
     <div
       className={`h-5 w-5 rounded-full border-[3px] border-white bg-primary-500 shadow-pin transition hover:border-primary-500 hover:bg-white`}
